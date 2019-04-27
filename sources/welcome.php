b06dd1558c4fe93fdd4d8dd49d674e326bee9248
Ferrenia <vinc.angel@hotmail.com>
2019-04-27 22:48:31 +0800

    Edit 1

--- sources/welcome.php
+++ sources/welcome.php
@@ -1,3 +1,21 @@
 <?php
 include_once ("./../pages/main.html");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Landing Page</title>
+</head>
+<body>
+<div class="page-title">Not Logged In</div>
+<br />
+<div class="page-option">
+    <form action="../index.php" method="get">
+        <button type="submit" class="page" name="login" value="login">Login</button>
+        <br />
+        <button type="submit" class="page" name="signup" value="reg">Registration</button>
+    </form>
+</div>
+</body>
+</html>
